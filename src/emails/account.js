91dda471094ea55be36a3efd39b3005e2d91f340
Leo Hsieh <leohsieh1001@gmail.com>
2022-09-16 14:48:25 +0800

    Initial commit

---
+++ src/emails/account.js
@@ -0,0 +1,29 @@
+const sgMail = require("@sendgrid/mail");
+
+// const sendgridAPIKey =
+//   "";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "leohsieh1001@gmail.com",
+    subject: "Thanks for joining in!",
+    text: `Hi ${name}, Welcome to the task manager app!`,
+  });
+};
+
+const sendGoobyeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "leohsieh1001@gmail.com",
+    subject: "Canclation completed",
+    text: `Hi ${name}, sorry to see you go, let me know if you have any suggestion!`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendGoobyeEmail,
+};
